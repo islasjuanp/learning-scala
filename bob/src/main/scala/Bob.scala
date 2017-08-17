c844346f4635fc3451482eabf2ef28bac232bcb5
Juan Pablo Islas <islasjuanp@gmail.com>
2017-08-17 00:31:05 -0300

    Added precompiled regular expression

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,8 +1,12 @@
 object Bob {
+  val question = ".*[?]+[\\s]*".r
+  val yell = "[^a-z]*[A-Z]+[^a-z]*".r
+  val silence = "[\\s]*".r
+
   def response(statement: String): String = statement match {
-    case str if str.matches("[^a-z]*[A-Z]+[^a-z]*") => "Whoa, chill out!"
-    case str if str.matches(".*[?]+[\\s]*") => "Sure."
-    case str if str.matches("[\\s]*") => "Fine. Be that way!"
+    case yell() => "Whoa, chill out!"
+    case question() => "Sure."
+    case silence() => "Fine. Be that way!"
     case _ => "Whatever."
   }
 }
